20c3ab70c0392cb17527c388adff1ee7dbdf6a13
Igor Kim <igkim90@gmail.com>
2019-12-11 14:09:33 +0500

    fix: grammar

---
+++ public/precache-manifest.f7ed5954dce07175cbc6a1370ad680dd.js
@@ -0,0 +1,218 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f8d2c1275601564bb51a0304825846d2",
+    "url": "/img/404.svg"
+  },
+  {
+    "revision": "e5aed37f0834f29b34404801f344680d",
+    "url": "/img/Artwork.svg"
+  },
+  {
+    "revision": "fa146c73e78cf04fc8e4ec9abb326691",
+    "url": "/img/about-us.png"
+  },
+  {
+    "revision": "17fd1f8e6e3bcbae9cf0c9c44970f0ef",
+    "url": "/img/adobe-illustrator-cc-seeklogo.com.svg"
+  },
+  {
+    "revision": "36dbda27d560f5e4f9e34514b606de64",
+    "url": "/img/adobe-photoshop-cc-seeklogo.com.svg"
+  },
+  {
+    "revision": "8fa0862703115b62d61ef9dd21e5c520",
+    "url": "/img/adobe-xd-seeklogo.com.svg"
+  },
+  {
+    "revision": "9719346f57ef4cd6aabbea225f6e0ad6",
+    "url": "/img/android-chrome-192x192.png"
+  },
+  {
+    "revision": "fe52e6815667ffd41a3a65972aa99e22",
+    "url": "/img/android-chrome-512x512.png"
+  },
+  {
+    "revision": "31559bbf0df0390cccf67a821580af7a",
+    "url": "/img/apple-touch-icon.png"
+  },
+  {
+    "revision": "6efcc5b45cd7630fb40220c544f55a1b",
+    "url": "/img/arrow.svg"
+  },
+  {
+    "revision": "c1d9fa5907487f0c84b19ef6a58003ff",
+    "url": "/img/bitbucket-seeklogo.com.svg"
+  },
+  {
+    "revision": "011c0d7278b2f7f0efe2a585e033905d",
+    "url": "/img/bootstrap-seeklogo.com.svg"
+  },
+  {
+    "revision": "51d5fc8241f320d9f71fbf4827d8534b",
+    "url": "/img/calendar.svg"
+  },
+  {
+    "revision": "ac604c9255d6bc273f1f02483950f7a1",
+    "url": "/img/cash.svg"
+  },
+  {
+    "revision": "09e0ab8bf2849425accec8cc2414bc8b",
+    "url": "/img/centos-seeklogo.com.svg"
+  },
+  {
+    "revision": "aafdeafa8712573b94200b7b1bffcef8",
+    "url": "/img/clojure-seeklogo.com.svg"
+  },
+  {
+    "revision": "294e5480a4d42b775b9cd595eccc1edd",
+    "url": "/img/css3-seeklogo.com.svg"
+  },
+  {
+    "revision": "0ac64f7695b9e70d23a4bb77ba167194",
+    "url": "/img/docker-seeklogo.com.svg"
+  },
+  {
+    "revision": "d52e7a6a9571462cf4f274a25b33c1fd",
+    "url": "/img/favicon-16x16.png"
+  },
+  {
+    "revision": "1f0c508768a92ac986437b6d1ddb7c3e",
+    "url": "/img/favicon-32x32.png"
+  },
+  {
+    "revision": "fc63b181956a3d1f0dd596167368b99d",
+    "url": "/img/favicon.ico"
+  },
+  {
+    "revision": "046e9c94dbc0431c0c1ed5912201eac3",
+    "url": "/img/figma-seeklogo.com.svg"
+  },
+  {
+    "revision": "2b168586eb5f960e5b03c9881f7df264",
+    "url": "/img/girl-big.png"
+  },
+  {
+    "revision": "f4761bd256b4021745245e2ac42de0c3",
+    "url": "/img/girl.png"
+  },
+  {
+    "revision": "14d7c583ee87ccc8ab9fe106af152423",
+    "url": "/img/git-seeklogo.com.svg"
+  },
+  {
+    "revision": "7bf82ce33ddc7e780dfbc5a1c9a7dbfd",
+    "url": "/img/github-seeklogo.com.svg"
+  },
+  {
+    "revision": "2a9b6bef75bc7f6d4170e779c879fda4",
+    "url": "/img/go-seeklogo.com.svg"
+  },
+  {
+    "revision": "64bbd71cdd6fb8885e9c56ef0fba7bba",
+    "url": "/img/igor.jpg"
+  },
+  {
+    "revision": "4efe202344a2d88a6833d21f13f6f31a",
+    "url": "/img/java-seeklogo.com.svg"
+  },
+  {
+    "revision": "5fde44ade9b84a501e3323469013fa7b",
+    "url": "/img/javascript-js-seeklogo.com.svg"
+  },
+  {
+    "revision": "c5ead0a83a8f6681ccca9d0f485ebf36",
+    "url": "/img/kotlin-seeklogo.com.svg"
+  },
+  {
+    "revision": "30348336d14f29b81454dfffcb920b09",
+    "url": "/img/logo.png"
+  },
+  {
+    "revision": "5a4f009518abf94bd74ef964883d4349",
+    "url": "/img/manPic.png"
+  },
+  {
+    "revision": "c109bc8054218c9490bff54c9faad24f",
+    "url": "/img/mstile-150x150.png"
+  },
+  {
+    "revision": "67387e35733941cb6905b89aaef64f84",
+    "url": "/img/nodejs-seeklogo.com.svg"
+  },
+  {
+    "revision": "372885251f82ebb42bffded00048831e",
+    "url": "/img/opp-guy.png"
+  },
+  {
+    "revision": "35e07c8d51574a311a840a93441c8a3e",
+    "url": "/img/profit-small.png"
+  },
+  {
+    "revision": "c9b85e1a3108d377a08a73e27e29d0d4",
+    "url": "/img/python-seeklogo.com.svg"
+  },
+  {
+    "revision": "d5ae5db20fee7c2ea8f5dc9737ea4b5b",
+    "url": "/img/rarr.svg"
+  },
+  {
+    "revision": "68590d291c0a36664b22a83a89ae4a86",
+    "url": "/img/react-seeklogo.com.svg"
+  },
+  {
+    "revision": "c2331729d5a0e151650a1bb96dcac1c4",
+    "url": "/img/safari-pinned-tab.svg"
+  },
+  {
+    "revision": "1fabe049747e9ac40fde4ea968deb73e",
+    "url": "/img/share.jpg"
+  },
+  {
+    "revision": "a3802684da1cf8859841f70c29469613",
+    "url": "/img/share.svg"
+  },
+  {
+    "revision": "4cd886d28323e0d84f783addea6c6943",
+    "url": "/img/sketch-seeklogo.com.svg"
+  },
+  {
+    "revision": "d95283854520691846fbb6bb1a18987b",
+    "url": "/img/small-footer-logo.png"
+  },
+  {
+    "revision": "d3e5d3c1b5de7362e30558d5095b9dbb",
+    "url": "/img/small_logo.png"
+  },
+  {
+    "revision": "e5e8fb449aa0e4893cb8274d3b4bdc4a",
+    "url": "/img/sqlite-seeklogo.com.svg"
+  },
+  {
+    "revision": "5c012ecb34735fa405b30da95f32b00d",
+    "url": "/img/start.svg"
+  },
+  {
+    "revision": "53a8c4bd78688001cfdbfec80a875e8a",
+    "url": "/img/time.svg"
+  },
+  {
+    "revision": "be3a683a844f7bbf462181a12eb33fd4",
+    "url": "/img/vuejs-seeklogo.com.svg"
+  },
+  {
+    "revision": "092b9cd1b1bce3670cbf",
+    "url": "/main.css"
+  },
+  {
+    "revision": "092b9cd1b1bce3670cbf",
+    "url": "/main.js"
+  },
+  {
+    "revision": "5e09a0a1c512f087285f",
+    "url": "/runtime.js"
+  },
+  {
+    "revision": "27a45db299c26b4eadab",
+    "url": "/vendors.js"
+  }
+]);
---
+++ public/sw.js
@@ -0,0 +1,19 @@
+importScripts("/precache-manifest.f7ed5954dce07175cbc6a1370ad680dd.js", "https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
+
+workbox.core.skipWaiting();
+workbox.core.clientsClaim();
+
+workbox.routing.registerRoute(
+  new workbox.strategies.StaleWhileRevalidate()
+);
+
+self.addEventListener('push', (event) => {
+  const title = 'Get Started With Workbox';
+  const options = {
+    body: event.data.text()
+  };
+  event.waitUntil(self.registration.showNotification(title, options));
+});
+
+workbox.precaching.precacheAndRoute(self.__precacheManifest);
+
